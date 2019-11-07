b54e54d1e843a35a4c40b3d1955fd9014f6d1f69
tan tran <tantv@vietmap.vn>
2019-11-07 10:16:28 +0700

    updateproduction

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,15 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   BASE_API: '"http://103.77.167.96:20001/api/"',
-  URL_IMAGE:'"http://103.77.167.96:20001/Avatars/"',  
+  URL_IMAGE:'"http://103.77.167.96:20001/Avatars/"', 
+  CONFIG : {
+    apiKey: '"AIzaSyA1v_6dJPgiHaPP1xlImYqnHihF-3jknmA"',
+    authDomain: '"pelodb.firebaseapp.com"',
+    databaseURL: '"https://pelodb.firebaseio.com"',
+    projectId: '"pelodb"',
+    storageBucket: '"pelodb.appspot.com"',
+    messagingSenderId: '"726134219557"',
+    appId: '"1:726134219557:web:325ff9635ab1f627dc70cc"',
+    measurementId: '"G-HBMJXC36ZX"'
+  }
 }
